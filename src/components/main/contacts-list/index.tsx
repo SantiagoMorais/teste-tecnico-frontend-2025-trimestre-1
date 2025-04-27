f6a9c5886f6d95534b79059a87d171b1afea278b
Felipe Santiago Morais <fillikz@gmail.com>
2025-04-27 16:39:17 -0300

    feat(ContactsList): ensure contacts will be render ordered by creation

--- src/components/main/contacts-list/index.tsx
+++ src/components/main/contacts-list/index.tsx
@@ -15,36 +15,42 @@ import { IContact } from "@/contexts/contacts-context/interfaces";
 
 import { ContactCardForms } from "./contact-card-forms";
 
-export const ContactsList = ({ contacts }: { contacts: IContact[] }) => (
-  <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    {contacts.map((contact) => (
-      <Card key={contact.id} className="gap-2 p-4">
-        <CardHeader className="p-0">
-          <CardTitle className="truncate capitalize">
-            {contact.username}
-          </CardTitle>
-          <CardDescription className="truncate first-letter:capitalize">
-            {contact.displayName}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="p-0">
-          <p>
-            <span className="font-semibold">CEP:</span> {contact.cep}
-          </p>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger className="line-clamp-2 w-full p-0 text-start">
-                <span className="font-semibold">Endereço:</span>{" "}
-                {contact.address}
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{contact.address}</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </CardContent>
-        <ContactCardForms contact={contact} />
-      </Card>
-    ))}
-  </ul>
-);
+export const ContactsList = ({ contacts }: { contacts: IContact[] }) => {
+  const sortedContacts = [...contacts].sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
+  return (
+    <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {sortedContacts.map((contact) => (
+        <Card key={contact.id} className="gap-2 p-4">
+          <CardHeader className="p-0">
+            <CardTitle className="truncate capitalize">
+              {contact.username}
+            </CardTitle>
+            <CardDescription className="truncate first-letter:capitalize">
+              {contact.displayName}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="p-0">
+            <p>
+              <span className="font-semibold">CEP:</span> {contact.cep}
+            </p>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="line-clamp-2 w-full p-0 text-start">
+                  <span className="font-semibold">Endereço:</span>{" "}
+                  {contact.address}
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{contact.address}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </CardContent>
+          <ContactCardForms contact={contact} />
+        </Card>
+      ))}
+    </ul>
+  );
+};
